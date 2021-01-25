98ae6ba481374e2d378897698964e61fc520feb0
Riccardo <riccardo@baako.de>
2021-01-25 22:46:39 +0100

    ex done

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const walk = 100; //100px
 
 function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero;
@@ -9,7 +10,11 @@ function shadow(e) {
     x = x + e.target.offsetLeft;
     y = y + e.target.offsetLeft;
   }
-  console.log(x,y);
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / width * walk) - (walk / 2));
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`
+
 }
 
 hero.addEventListener('mousemove', shadow);
